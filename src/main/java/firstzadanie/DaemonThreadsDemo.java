9f95d965aab760bfe36b7464f377a0cdc38edec9
Иванов Глеб <gleb4832@mail.ru>
2026-02-20 16:56:46 +0400

    1-1

---
+++ src/main/java/firstzadanie/DaemonThreadsDemo.java
@@ -0,0 +1,59 @@
+package firstzadanie;
+
+import java.io.File;
+import java.io.IOException;
+
+public class DaemonThreadsDemo implements Runnable {
+
+    public void run() {
+        System.out.println("\nЗадание 1: Потоки-демоны");
+
+        Thread daemonThread = new Thread(() -> {
+            try {
+                int count = 0;
+                while (true) {
+                    File tempFile = File.createTempFile("demo", ".tmp");
+                    System.out.println("[Демон] Создан временный файл: " + tempFile.getName());
+
+                    if (tempFile.delete()) {
+                        System.out.println("[Демон] Файл удален.");
+                    }
+
+                    Thread.sleep(5000);
+                }
+            } catch (InterruptedException | IOException e) {
+                System.out.println("[Демон] Прерван или ошибка ввода-вывода.");
+            }
+        });
+        daemonThread.setDaemon(true);
+        daemonThread.start();
+
+        Thread userThread = new Thread(() -> {
+            try {
+                for (int i = 1; i <= 5; i++) {
+                    System.out.println("[Обычный] Сообщение #" + i);
+                    Thread.sleep(2000);
+                }
+                System.out.println("[Обычный] Завершил работу.");
+            } catch (InterruptedException e) {
+                System.out.println("[Обычный] Прерван.");
+            }
+        });
+
+        userThread.start();
+
+        try {
+            userThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("[Main] Основной поток завершен. Демоны должны прекратить работу автоматически.");
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ignored) {}
+
+        System.out.println("--- Конец задания 1 ---");
+    }
+}
---
+++ src/main/java/secondZadanie/UncaughtExceptionDemo.java
@@ -0,0 +1,49 @@
+package secondZadanie;
+
+import java.util.Random;
+
+public class UncaughtExceptionDemo implements Runnable{
+
+    public void run() {
+        System.out.println("\nЗадание 2: Обработчики неконтролируемых исключений");
+
+        ThreadGroup group = new ThreadGroup("Group2") {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("[Групповой обработчик] Поймано исключение в потоке '" + t.getName() + "': " + e.getMessage());
+            }
+        };
+
+        Thread threadWithInternalHandler = new Thread(group, () -> {
+            try {
+                Random rand = new Random();
+                if (rand.nextBoolean()) {
+                    throw new RuntimeException("Случайное исключение ВНУТРИ потока (обработано локально)");
+                }
+                System.out.println("[Внутренний] Поток выполнился без исключений.");
+            } catch (RuntimeException e) {
+                System.out.println("[Внутренний] Исключение перехвачено внутри потока: " + e.getMessage());
+            }
+        }, "Thread-Internal");
+
+        Thread threadWithGroupHandler = new Thread(group, () -> {
+            Random rand = new Random();
+            if (rand.nextBoolean()) {
+                throw new RuntimeException("Случайное исключение в потоке (обработано группой)");
+            }
+            System.out.println("[Групповой] Поток выполнился без исключений.");
+        }, "Thread-GroupHandled");
+
+        threadWithInternalHandler.start();
+        threadWithGroupHandler.start();
+
+        try {
+            threadWithInternalHandler.join();
+            threadWithGroupHandler.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Конец задания 2");
+    }
+}
